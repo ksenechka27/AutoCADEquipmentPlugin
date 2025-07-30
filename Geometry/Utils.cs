3da370c7a2cc1ba9fc8dfc0a12fb85c43f48706e
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-30 17:36:19 +0300

    Update Utils.cs

--- Geometry/Utils.cs
+++ Geometry/Utils.cs
@@ -1,11 +1,44 @@
-using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
+using System.Collections.Generic;
 
 namespace AutoCADEquipmentPlugin.Geometry
 {
     public static class GeometryUtils
     {
+        // Проверка: точка внутри замкнутой полилинии
+        public static bool IsPointInside(Polyline polyline, Point3d point)
+        {
+            var pts = new Point3dCollection();
+            for (int i = 0; i < polyline.NumberOfVertices; i++)
+            {
+                pts.Add(polyline.GetPoint3dAt(i));
+            }
+
+            return IsPointInside(pts, point);
+        }
+
+        // Вспомогательный метод: точка в многоугольнике (Ray Casting)
+        public static bool IsPointInside(Point3dCollection polygon, Point3d point)
+        {
+            int crossings = 0;
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                Point3d a = polygon[i];
+                Point3d b = polygon[(i + 1) % polygon.Count];
+
+                if (((a.Y <= point.Y) && (b.Y > point.Y)) || ((a.Y > point.Y) && (b.Y <= point.Y)))
+                {
+                    double vt = (double)(point.Y - a.Y) / (b.Y - a.Y);
+                    double intersectX = a.X + vt * (b.X - a.X);
+                    if (point.X < intersectX)
+                        crossings++;
+                }
+            }
+            return (crossings % 2) != 0;
+        }
+
+        // Проверка пересечений с другими объектами в модели
         public static bool IntersectsOther(BlockTableRecord ms, BlockReference br, Transaction tr)
         {
             if (br.Bounds == null) return false;
