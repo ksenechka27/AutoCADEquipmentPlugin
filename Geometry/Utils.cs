86016a6373c4fc7a575bfa5a36404c7658cbdbd4
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-30 16:55:41 +0300

    Update Utils.cs

--- Geometry/Utils.cs
+++ Geometry/Utils.cs
@@ -3,37 +3,40 @@ using Autodesk.AutoCAD.Geometry;
 using System;
 using System.Collections.Generic;
 
-public static class GeometryUtils
+namespace AutoCADEquipmentPlugin.Geometry
 {
-    public static bool IsPointInside(this Polyline pline, Point2d pt, Tolerance tolerance)
+    public static class GeometryUtils
     {
-        if (!pline.Closed) return false;
-
-        int windingNumber = 0;
-        int numVerts = pline.NumberOfVertices;
-
-        for (int i = 0; i < numVerts; i++)
+        public static bool IsPointInside(this Polyline pline, Point2d pt, Tolerance tolerance)
         {
-            Point2d p1 = pline.GetPoint2dAt(i);
-            Point2d p2 = pline.GetPoint2dAt((i + 1) % numVerts);
+            if (!pline.Closed) return false;
 
-            if (p1.Y <= pt.Y)
+            int windingNumber = 0;
+            int numVerts = pline.NumberOfVertices;
+
+            for (int i = 0; i < numVerts; i++)
             {
-                if (p2.Y > pt.Y && IsLeft(p1, p2, pt) > 0)
-                    windingNumber++;
+                Point2d p1 = pline.GetPoint2dAt(i);
+                Point2d p2 = pline.GetPoint2dAt((i + 1) % numVerts);
+
+                if (p1.Y <= pt.Y)
+                {
+                    if (p2.Y > pt.Y && IsLeft(p1, p2, pt) > 0)
+                        windingNumber++;
+                }
+                else
+                {
+                    if (p2.Y <= pt.Y && IsLeft(p1, p2, pt) < 0)
+                        windingNumber--;
+                }
             }
-            else
-            {
-                if (p2.Y <= pt.Y && IsLeft(p1, p2, pt) < 0)
-                    windingNumber--;
-            }
+
+            return windingNumber != 0;
         }
 
-        return windingNumber != 0;
-    }
-
-    private static double IsLeft(Point2d p0, Point2d p1, Point2d p2)
-    {
-        return (p1.X - p0.X) * (p2.Y - p0.Y) - (p2.X - p0.X) * (p1.Y - p0.Y);
+        private static double IsLeft(Point2d p0, Point2d p1, Point2d p2)
+        {
+            return (p1.X - p0.X) * (p2.Y - p0.Y) - (p2.X - p0.X) * (p1.Y - p0.Y);
+        }
     }
 }
