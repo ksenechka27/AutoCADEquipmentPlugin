40d8d89632b070feb70525db3fa5e8b5b54cd74b
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-30 12:35:00 +0300

    Update Utils.cs

--- Geometry/Utils.cs
+++ Geometry/Utils.cs
@@ -1,42 +1,38 @@
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
-using Autodesk.AutoCAD.EditorInput;
-using System;
 
 namespace AutoCADEquipmentPlugin.Geometry
 {
     public static class Utils
     {
-        public static bool IsPointInside(Polyline poly, Point3d point)
+        public static bool IsPointInside(Polyline poly, Point3d p)
         {
-            return poly.GetDistAtPoint(poly.GetClosestPointTo(point, false)) >= 0;
+            return poly.GetArea() > 0 && poly.IsPointInside(p, new Tolerance(1e-6, 1e-4), true);
         }
 
-        public static bool IntersectsOtherObjects(BlockTableRecord modelSpace, BlockReference br, Transaction tr)
+        public static bool IntersectsOther(BlockTableRecord ms, BlockReference br, Transaction tr)
         {
-            if (!br.Bounds.HasValue) return false;
-            Extents3d brExt = br.Bounds.Value;
+            var bb = br.Bounds;
+            if (!bb.HasValue) return false;
 
-            foreach (ObjectId entId in modelSpace)
+            foreach (var oid in ms)
             {
-                if (entId == br.ObjectId) continue;
-
-                Entity ent = tr.GetObject(entId, OpenMode.ForRead) as Entity;
-                if (ent == null || ent is BlockReference == false || !ent.Bounds.HasValue) continue;
-
-                Extents3d entExt = ent.Bounds.Value;
-
-                if (IsOverlap(brExt, entExt))
-                    return true;
+                if (oid == br.ObjectId) continue;
+                var ent = tr.GetObject(oid, OpenMode.ForRead) as Entity;
+                if (ent is BlockReference obr)
+                {
+                    var ob = obr.Bounds;
+                    if (ob.HasValue &&
+                        bb.Value.MinPoint.X < ob.Value.MaxPoint.X &&
+                        bb.Value.MaxPoint.X > ob.Value.MinPoint.X &&
+                        bb.Value.MinPoint.Y < ob.Value.MaxPoint.Y &&
+                        bb.Value.MaxPoint.Y > ob.Value.MinPoint.Y)
+                    {
+                        return true;
+                    }
+                }
             }
-
             return false;
-        }
-
-        private static bool IsOverlap(Extents3d a, Extents3d b)
-        {
-            return !(a.MaxPoint.X < b.MinPoint.X || a.MinPoint.X > b.MaxPoint.X ||
-                     a.MaxPoint.Y < b.MinPoint.Y || a.MinPoint.Y > b.MaxPoint.Y);
         }
     }
 }
