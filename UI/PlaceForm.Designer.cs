a067b1f1f68c5ae8180d002311518295dea65d2f
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-31 17:22:20 +0300

    Update PlaceForm.Designer.cs

--- UI/PlaceForm.Designer.cs
+++ UI/PlaceForm.Designer.cs
@@ -3,92 +3,73 @@ namespace AutoCADEquipmentPlugin.UI
     partial class PlaceForm
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.TextBox txtBlockName;
-        private System.Windows.Forms.NumericUpDown numOffset;
+
         private System.Windows.Forms.Label lblBlockName;
-        private System.Windows.Forms.Label lblOffset;
-        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtBlockName;
+        private System.Windows.Forms.Button btnPlace;
         private System.Windows.Forms.Button btnCancel;
 
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         private void InitializeComponent()
         {
-            this.txtBlockName = new System.Windows.Forms.TextBox();
-            this.numOffset = new System.Windows.Forms.NumericUpDown();
             this.lblBlockName = new System.Windows.Forms.Label();
-            this.lblOffset = new System.Windows.Forms.Label();
-            this.btnOk = new System.Windows.Forms.Button();
+            this.txtBlockName = new System.Windows.Forms.TextBox();
+            this.btnPlace = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numOffset)).BeginInit();
             this.SuspendLayout();
-
-            // txtBlockName
-            this.txtBlockName.Location = new System.Drawing.Point(120, 20);
-            this.txtBlockName.Name = "txtBlockName";
-            this.txtBlockName.Size = new System.Drawing.Size(200, 23);
-
-            // numOffset
-            this.numOffset.Location = new System.Drawing.Point(120, 60);
-            this.numOffset.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
-            this.numOffset.Minimum = new decimal(new int[] { 0, 0, 0, 0 });
-            this.numOffset.Name = "numOffset";
-            this.numOffset.Size = new System.Drawing.Size(200, 23);
-            this.numOffset.Value = new decimal(new int[] { 100, 0, 0, 0 });
 
             // lblBlockName
             this.lblBlockName.AutoSize = true;
-            this.lblBlockName.Location = new System.Drawing.Point(20, 23);
+            this.lblBlockName.Location = new System.Drawing.Point(12, 15);
             this.lblBlockName.Name = "lblBlockName";
-            this.lblBlockName.Size = new System.Drawing.Size(90, 15);
+            this.lblBlockName.Size = new System.Drawing.Size(92, 13);
+            this.lblBlockName.TabIndex = 0;
             this.lblBlockName.Text = "Имя блока:";
 
-            // lblOffset
-            this.lblOffset.AutoSize = true;
-            this.lblOffset.Location = new System.Drawing.Point(20, 62);
-            this.lblOffset.Name = "lblOffset";
-            this.lblOffset.Size = new System.Drawing.Size(74, 15);
-            this.lblOffset.Text = "Отступ (мм):";
+            // txtBlockName
+            this.txtBlockName.Location = new System.Drawing.Point(110, 12);
+            this.txtBlockName.Name = "txtBlockName";
+            this.txtBlockName.Size = new System.Drawing.Size(200, 20);
+            this.txtBlockName.TabIndex = 1;
 
-            // btnOk
-            this.btnOk.Location = new System.Drawing.Point(120, 100);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(90, 30);
-            this.btnOk.Text = "OK";
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // btnPlace
+            this.btnPlace.Location = new System.Drawing.Point(110, 50);
+            this.btnPlace.Name = "btnPlace";
+            this.btnPlace.Size = new System.Drawing.Size(95, 23);
+            this.btnPlace.TabIndex = 2;
+            this.btnPlace.Text = "Разместить";
+            this.btnPlace.UseVisualStyleBackColor = true;
+            this.btnPlace.Click += new System.EventHandler(this.btnPlace_Click);
 
             // btnCancel
-            this.btnCancel.Location = new System.Drawing.Point(230, 100);
+            this.btnCancel.Location = new System.Drawing.Point(215, 50);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(90, 30);
+            this.btnCancel.Size = new System.Drawing.Size(95, 23);
+            this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
 
             // PlaceForm
-            this.AcceptButton = this.btnOk;
-            this.CancelButton = this.btnCancel;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.ClientSize = new System.Drawing.Size(350, 150);
-            this.Controls.Add(this.txtBlockName);
-            this.Controls.Add(this.numOffset);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 90);
             this.Controls.Add(this.lblBlockName);
-            this.Controls.Add(this.lblOffset);
-            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtBlockName);
+            this.Controls.Add(this.btnPlace);
             this.Controls.Add(this.btnCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PlaceForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Параметры размещения";
-            ((System.ComponentModel.ISupportInitialize)(this.numOffset)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Размещение оборудования";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
