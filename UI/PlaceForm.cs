2e3a4a46f5cfbbf699713f301fdc1d3a50282630
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-31 10:31:18 +0300

    Update PlaceForm.cs

--- UI/PlaceForm.cs
+++ UI/PlaceForm.cs
@@ -1,120 +1,92 @@
-using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
-using Autodesk.AutoCAD.Runtime;
-using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AutoCADEquipmentPlugin.Logic
 {
-    public static class Placer
+    public static class Utils
     {
-        public static void Place(string blockName, double offset, bool clearOld)
+        // Получить габариты блока
+        public static Extents3d? GetBlockExtents(BlockReference br)
         {
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed = doc.Editor;
-            Database db = doc.Database;
-
-            using (Transaction tr = db.TransactionManager.StartTransaction())
+            try
             {
-                // Очистка старых блоков
-                if (clearOld)
-                {
-                    Utils.ClearPreviousBlocks(blockName, tr);
-                }
-
-                // Выбор полилинии области расстановки
-                PromptEntityOptions peo = new PromptEntityOptions("\nВыберите внутреннюю полилинию зоны расстановки:");
-                peo.SetRejectMessage("Только полилиния.");
-                peo.AddAllowedClass(typeof(Polyline), true);
-                PromptEntityResult per = ed.GetEntity(peo);
-                if (per.Status != PromptStatus.OK) return;
-                Polyline zonePolyline = tr.GetObject(per.ObjectId, OpenMode.ForRead) as Polyline;
-                if (!zonePolyline.Closed)
-                {
-                    ed.WriteMessage("\nПолилиния должна быть замкнута.");
-                    return;
-                }
-
-                // Ввод начальной и конечной точки пользователем
-                PromptPointResult startRes = ed.GetPoint("\nУкажите начальную точку размещения:");
-                if (startRes.Status != PromptStatus.OK) return;
-                Point3d startPoint = startRes.Value;
-
-                PromptPointResult endRes = ed.GetPoint("\nУкажите конечную точку размещения:");
-                if (endRes.Status != PromptStatus.OK) return;
-                Point3d endPoint = endRes.Value;
-
-                // Получение определения блока
-                BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
-                if (!bt.Has(blockName))
-                {
-                    ed.WriteMessage("\nБлок с именем " + blockName + " не найден.");
-                    return;
-                }
-                BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[blockName], OpenMode.ForRead);
-
-                // Границы блока
-                Extents3d? blockExt = Utils.GetBlockExtents(btr);
-                if (blockExt == null)
-                {
-                    ed.WriteMessage("\nНевозможно определить габариты блока.");
-                    return;
-                }
-
-                Vector3d totalDirection = endPoint - startPoint;
-
-                List<SegmentInfo> segments = Utils.ExtractSegments(zonePolyline);
-                Point3d currentPos = startPoint;
-                Vector3d moveDir = totalDirection.GetNormal();
-
-                foreach (var seg in segments)
-                {
-                    Vector3d segDir = (seg.End - seg.Start).GetNormal();
-                    double segmentLength = seg.Start.DistanceTo(seg.End);
-                    Point3d blockPos = seg.Start;
-
-                    // Ориентируем блок вдоль сегмента
-                    double angle = segDir.AngleOnPlane(new Plane(Point3d.Origin, Vector3d.ZAxis));
-                    double step = blockExt.Value.MaxPoint.X - blockExt.Value.MinPoint.X + offset;
-
-                    while (blockPos.DistanceTo(seg.End) >= step)
-                    {
-                        // Проверка попадания внутрь зоны
-                        if (!Utils.IsPointInside(zonePolyline, blockPos))
-                        {
-                            blockPos = blockPos + segDir.MultiplyBy(step);
-                            continue;
-                        }
-
-                        // Проверка на пересечения с объектами
-                        if (Utils.HasIntersections(blockPos, angle, btr, tr))
-                        {
-                            blockPos = blockPos + segDir.MultiplyBy(step);
-                            continue;
-                        }
-
-                        // Вставка блока
-                        BlockReference br = new BlockReference(blockPos, btr.ObjectId);
-                        br.Rotation = angle;
-                        BlockTableRecord modelSpace = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
-                        modelSpace.AppendEntity(br);
-                        tr.AddNewlyCreatedDBObject(br, true);
-
-                        blockPos = blockPos + segDir.MultiplyBy(step);
-                    }
-                }
-
-                tr.Commit();
+                var ext = br.GeometricExtents;
+                return ext;
+            }
+            catch
+            {
+                return null;
             }
         }
 
-        private struct SegmentInfo
+        // Преобразовать полилинию в список отрезков
+        public static List<LineSegment2d> ExtractSegments(Polyline pline)
         {
-            public Point3d Start;
-            public Point3d End;
+            var segments = new List<LineSegment2d>();
+            for (int i = 0; i < pline.NumberOfVertices; i++)
+            {
+                int next = (i + 1) % pline.NumberOfVertices;
+                var p1 = pline.GetPoint2dAt(i);
+                var p2 = pline.GetPoint2dAt(next);
+                segments.Add(new LineSegment2d(p1, p2));
+            }
+            return segments;
+        }
+
+        // Проверка: точка находится внутри замкнутой полилинии
+        public static bool IsPointInside(Polyline pline, Point2d pt)
+        {
+            return pline.IsPointInside(pt, Tolerance.Global, true);
+        }
+
+        // Проверка: будет ли вставка блока пересекаться с другими объектами
+        public static bool HasIntersections(Transaction tr, BlockReference newBlock, BlockTableRecord btr, Polyline boundary)
+        {
+            var db = HostApplicationServices.WorkingDatabase;
+            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            // Получаем предполагаемый экстент вставляемого блока
+            var ext = GetBlockExtents(newBlock);
+            if (ext == null) return true;
+
+            // Получаем центр блока для быстрой проверки
+            var center = (ext.Value.MinPoint + ext.Value.MaxPoint) / 2.0;
+            if (!IsPointInside(boundary, new Point2d(center.X, center.Y)))
+                return true;
+
+            // Пройтись по всем объектам и проверить пересечение
+            foreach (ObjectId id in btr)
+            {
+                var ent = tr.GetObject(id, OpenMode.ForRead) as Entity;
+                if (ent is BlockReference existingBr)
+                {
+                    if (existingBr.GeometricExtents.IntersectWith(ext.Value) != null)
+                        return true;
+                }
+                else if (ent != null && ent.Bounds != null)
+                {
+                    if (ent.Bounds.Value.IntersectWith(ext.Value) != null)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Угол между двумя точками (в радианах)
+        public static double GetAngle(Point2d from, Point2d to)
+        {
+            return (to - from).Angle;
+        }
+
+        // Создаёт нормализованный вектор длины length по сегменту
+        public static Vector2d GetDirection(LineSegment2d seg)
+        {
+            return (seg.EndPoint - seg.StartPoint).GetNormal();
         }
     }
 }
