1605ac927615faf9ad0024e345b31ff14718d4a3
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-31 11:14:55 +0300

    Update Placer.cs

--- Logic/Placer.cs
+++ Logic/Placer.cs
@@ -5,7 +5,7 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
-using AutoCADEquipmentPlugin.Utils;
+using AutoCADEquipmentPlugin.Geometry;
 
 namespace AutoCADEquipmentPlugin.Logic
 {
