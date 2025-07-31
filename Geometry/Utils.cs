e7e09f87b6ffbc3c9d43ff23755359db9c732e9d
ksenechka27 <ignatovaksenechka@gmail.com>
2025-07-31 16:45:10 +0300

    Update Utils.cs

--- Geometry/Utils.cs
+++ Geometry/Utils.cs
@@ -5,44 +5,47 @@ namespace AutoCADEquipmentPlugin.Geometry
 {
     public static class GeometryUtils
     {
-        /// <summary>
-        /// Проверка, находится ли точка внутри полилинии.
-        /// </summary>
-        public static bool IsPointInside(Polyline poly, Point3d pt)
+        /// <summary>Проверка, находится ли точка внутри 2D-полилинии.</summary>
+        public static bool IsPointInside(this Polyline poly, Point3d pt)
         {
-            Plane plane = new Plane(Point3d.Origin, Vector3d.ZAxis); // по XY-плоскости
-            Point2d pt2d = pt.Convert2d(plane);
-            return poly.IsPointInside(pt2d, Tolerance.Global, false);
+            // Алгоритм "ray casting" по Y
+            var point2d = new Point2d(pt.X, pt.Y);
+            int crossings = 0;
+            for (int i = 0; i < poly.NumberOfVertices; i++)
+            {
+                Point2d a = poly.GetPoint2dAt(i);
+                Point2d b = poly.GetPoint2dAt((i + 1) % poly.NumberOfVertices);
+
+                if (((a.Y <= point2d.Y && point2d.Y < b.Y) || (b.Y <= point2d.Y && point2d.Y < a.Y)) &&
+                    (point2d.X < (b.X - a.X) * (point2d.Y - a.Y) / (b.Y - a.Y + 1e-9) + a.X))
+                {
+                    crossings++;
+                }
+            }
+            return (crossings % 2) == 1;
         }
 
-        /// <summary>
-        /// Проверка пересечения блока с другими объектами (по габаритам).
-        /// </summary>
+        /// <summary>Проверка пересечения блока с другими объектами по габаритам.</summary>
         public static bool IntersectsOther(BlockTableRecord ms, BlockReference br, Transaction tr)
         {
             if (!br.Bounds.HasValue) return false;
-
-            Extents3d brBounds = br.Bounds.Value;
+            Extents3d bb = br.Bounds.Value;
 
             foreach (ObjectId id in ms)
             {
                 if (id == br.ObjectId) continue;
-
                 Entity ent = tr.GetObject(id, OpenMode.ForRead) as Entity;
                 if (ent == null || !ent.Bounds.HasValue) continue;
 
-                Extents3d entBounds = ent.Bounds.Value;
-
-                bool intersects =
-                    brBounds.MinPoint.X <= entBounds.MaxPoint.X &&
-                    brBounds.MaxPoint.X >= entBounds.MinPoint.X &&
-                    brBounds.MinPoint.Y <= entBounds.MaxPoint.Y &&
-                    brBounds.MaxPoint.Y >= entBounds.MinPoint.Y;
-
-                if (intersects)
+                Extents3d eb = ent.Bounds.Value;
+                if (bb.MinPoint.X <= eb.MaxPoint.X &&
+                    bb.MaxPoint.X >= eb.MinPoint.X &&
+                    bb.MinPoint.Y <= eb.MaxPoint.Y &&
+                    bb.MaxPoint.Y >= eb.MinPoint.Y)
+                {
                     return true;
+                }
             }
-
             return false;
         }
 
